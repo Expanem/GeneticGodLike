9557f4ed1ff85c015b6c72b7ebb351d5885aed65
Goneiross <guillaume.julien.pellerin@gmail.com>
2020-06-07 20:26:17 +0200

    Add simulation and update makefile

---
+++ src/simulation.cc
@@ -0,0 +1,14 @@
+/*!
+  \file   simulation.cc
+  \date   June 2020
+  \brief  simulation module
+*/
+
+#include "simulation.h"
+#include "world.h"
+
+using namespace std;
+
+void main_loop() {
+
+}
---
+++ src/simulation.h
@@ -0,0 +1,12 @@
+/*!
+  \file   simulation.h
+  \date   June 2020
+  \brief  simulation header
+*/
+
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+void main_loop();
+
+#endif
